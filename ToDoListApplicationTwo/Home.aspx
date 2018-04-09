0610b6492d183c9ffd51471f931969af64d7716f
alexscodecave <alexbiddle72@gmail.com>
2018-04-09 21:44:46 +0100

    To do list project
    A to do list project I am working on to try and help improve my knowledge and skills of jQuery and Web Services. Including methods such as draggable, droppable and an acommpanying web service where I can add my own notes, edit them and update them. I have plans in the future to expand on the project so I can create and name what I call "pockets" where users can sort their notes in a friendly manner.

---
+++ ToDoListApplicationTwo/Home.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="ToDoListApplicationTwo.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/jquery-3.3.1.min.js"></script>
+    <script src="Scripts/jquery-ui-1.12.1.min.js"></script>
+    <link href="Content/jquery-ui.min.css" rel="stylesheet" />
+    
+    <link href="Content/font-awesome.min.css" rel="stylesheet" />
+    <link href="Home.css" rel="stylesheet" />
+    <script src="Scripts/jquery-confirm.js"></script>
+    <script src="Home.js"></script>
+    <title>To Do List</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container-fluid outsideContainer">
+            <asp:Button runat="server" ID="btnGoToRegister" OnClick="btnGoToRegister_OnClick"/>
+            <h3 class="text-center">To-Do List</h3>
+            
+            <div class="row">
+                <textarea class="textNote" placeholder="Enter" a note here" cols="152" rows="5"></textarea>
+
+            </div>
+            <div class="row noteRow">
+                
+            </div>
+            <div class="row">
+                <button type="button" class="btn btn-primary btnSubmitDefaultNote">Submit note</button>
+            </div>
+                <h3 class="text-center"> Your notes</h3>
+                <div class="row myNotes">
+                    
+                </div>
+            </div>
+        <div class="noteOptions">
+            <div class="row">
+                <button type="button" class="btn btn-primary col-md-4 ">Delete</button>
+                <div class="col-md-4"></div>
+                <button type="button" class="btn btn-primary col-md-4">Edit</button>
+            </div>
+        </div>
+        
+        <div class="notePockets">
+            <h3 class="text-center">Your pockets</h3>
+            
+            <div class="row">
+                
+                <div class="col-md-2  pocket greenPocket">
+                    
+                </div>
+                <div class="offset-1"></div>
+                <div class="col-md-2 pocket redPocket droppable">    
+                </div>
+                <div class="offset-2"></div>
+                <div class="col-md-2 pocket bluePocket droppable"><div class="offset-1"></div>
+                </div>
+                <div class="offset-1"></div>
+                <div class="col-md-2 pocket yellowPocket droppable">
+                    
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="footer-copyright py-3">
+            <div class="pull-left">On the left side</div>
+            <div class="text-center">2018 Copyright</div>
+            <div class="pull-right">On the right side</div>
+        </div>
+       
+    </form>
+</body>
+</html>
---
+++ ToDoListApplicationTwo/Home.js
@@ -0,0 +1,200 @@
+﻿$(document).ready(function () {
+    retrieveNotesFromDB(); //retrieve previously added notes from the database
+
+    $(".testList li").eq(2).css("background-color", "red");
+
+    $(".droppable").droppable({ //all pockets now have this class
+        drop: function (event, ui) {
+
+        },
+        activate: function(event, ui) {
+            $(".droppable").toggleClass("blueBorder");
+        },
+        //over function of droppable is raised once on the droppable
+        //on the other hand the drag event of the draggle is raised
+        //every time the mouse movess
+        over: function (event, ui) {
+            //convert the css value of the draggable element that is 
+            //hovered over the droppable into an int
+            var parseDraggableElement = parseInt(ui.draggable.css("height"));
+            //convert height of the droppable to an int
+            var parseDroppableElement = parseInt($(this).css("height"));
+
+            var addDragHeiToDrop = parseDraggableElement + parseDroppableElement;
+            console.log("After converting: " + addDragHeiToDrop);
+            ui.draggable.toggleClass("toggledHeightAnimation");
+
+            if (ui.draggable.hasClass("toggledHeightAnimation")) {
+                $(this).animate({
+                    height: addDragHeiToDrop + "px"
+                });   
+            }
+            else {
+                $(this).animate({
+                    height: "100px"
+                });
+            }
+        }
+    });
+
+
+    $('[class$="btnSubmitDefaultNote"]').on("click",
+        function () {
+            var getNoteContent = $('[class^="textNote"]').val(); //every class that starts textNote
+            if ($(getNoteContent).val() == "") { //ensure textarea is empty and does not contain whitespace
+                alert("Please ensure the note has a value inside of it");
+            } else {
+                AddNoteToDB(getNoteContent);
+            }
+        });
+
+    $("#draggable").on("hover", function () {
+
+    })
+
+    $('[class$="outsideContainer"]').on("click", ".dynamicNote ",
+        function () {
+            var getNoteDataAttribute = ($(this).attr("data-note-from-db"));
+            changeDivToTextArea(getNoteDataAttribute);
+            $(this).draggable();
+        });
+
+    function changeDivToTextArea(noteDataID) {
+        console.log("This notes unique ID: " + noteDataID);
+
+        console.log("Found it: " + noteDataID);
+
+        //found it by unique id, must be because the class is dynamically loading
+        //need to find way to be able to search by dynamically created class
+
+        //here we are selecting every element inside of the class myNotes which is a div and has a data attribute
+        //data-note-from-db equal to the value passed through in the parameter
+        var getDynamicNote = $('.myNotes div[data-note-from-db="' + noteDataID + '"]');
+        //here we get the text of the one selected
+        var dynamicNoteText = $('.myNotes div[data-note-from-db="' + noteDataID + '"]').text();
+
+        //get text div element that is dynamically created
+        var textArea = $("<textarea style='width:180px;height:180px;'/>");
+        textArea.val(dynamicNoteText);
+        getDynamicNote.replaceWith(textArea); //may need data attribute here to uniquely
+        //identify which of the  
+        textArea.focus(); //focus on textarea
+
+        $(textArea).on("blur",
+            function () {
+                //preserver value of textarea
+                var html = $(this).val();
+
+                //create a dynamic div
+                var viewableText =
+                    $("<div class='draggable dynamicNote styleDynamicDiv draggableNote'></div>");
+                //set its html
+                viewableText.html(html);
+                //replace out the textarea
+                $(this).replaceWith(viewableText);
+
+                $(".draggableNote").draggable({ //elements with the class draggableNote have been given the capability to be dragged ;)
+                    appendTo: "body",
+                    zIndex: 10000,
+                    snap: ".pocket",
+                    stack: ".draggableNote",
+                    revert: "invalid",
+                    start: function () {
+                        console.log("Dynamic note has started dragging"); // yyayayayya it works
+                    },
+                    stop: function () {
+                        console.log("Dynamic note has stopped dragging"); //show this to console when dragging has stopped
+                    }
+                });
+                genericAjax("updateNote", noteDataID, html);
+            });
+
+    }
+
+
+    function genericAjax(urlMethod, noteID, noteContent) {
+        var myData = JSON.stringify({
+            noteID: noteID,
+            noteContent: noteContent
+        });
+
+        $.ajax({
+            type: "POST",
+            url: "ToDoListService.asmx/" + urlMethod,
+            dataType: "json",
+            data: myData,
+            contentType: "application/json",
+            success: function (msg) {
+                var msgData = msg.d;
+                console.log("Updated in database");
+            },
+            failure: function (msg) {
+                console.log(msg.responseText);
+            },
+            error: function (msg) {
+                console.log(msg.responseText);
+            }
+        });
+    }
+
+    function AddNoteToDB(noteContent) {
+        var myData = JSON.stringify({
+            noteContent: noteContent
+        });
+        $.ajax({
+            type: "POST",
+            url: "ToDoListService.asmx/AddNoteToDB",
+            dataType: "json",
+            data: myData,
+            contentType: "application/json",
+            success: function (msg) {
+                var msgData = msg.d;
+                console.log(msg.responseText);
+                alert("Record added to database");
+            },
+            failure: function (msg) {
+                console.log(msg.responseText);
+            },
+            error: function (msg) {
+                console.log(msg.responseText);
+            }
+        });
+    }
+
+
+
+    function retrieveNotesFromDB() {
+        $.ajax({
+            type: "POST",
+            url: "ToDoListService.asmx/retrieveNotes",
+            dataType: "json",
+            data: "",
+            contentType: "application/json",
+            success: function (msg) {
+                var msgData = msg.d;
+
+                $.each(msgData,
+                    function (index, value) {
+                        var containerHTML = '<div class="row col-md-2">';
+                        containerHTML =
+                            containerHTML +
+                            '<div class="draggable dynamicNote makeBorderEdgesCurved styleDynamicDiv draggable makeDivBoxThreeD" data-note-from-db=' +
+                            (index + 1) +
+                            '>' +
+                            value.NoteContent +
+                            '</div></div>';
+                        $(".myNotes").append(containerHTML);
+
+
+
+                    });
+            },
+            failure: function (msg) {
+                console.log(msg.responseText);
+            },
+            error: function (msg) {
+                console.log(msg.responseText);
+            }
+        });
+    }
+})
